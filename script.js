5f16b0cbfc2cea281834dbd69c9c2f9eafddaf7e
samsundar012 <a.samsundar12@gmail.com>
2023-11-14 22:49:11 +0530

    update

---
+++ script.js
@@ -0,0 +1,34 @@
+/*let jsonData = {
+    "name": "sam",
+    "age": "25",
+    "city": "madurai"
+  };
+  
+  for (let key in jsonData) {
+    if (jsonData.hasOwnProperty(key)) {
+      console.log(key + ": " + jsonData[key]);
+    }
+  }*/
+  
+
+ /* let language = "JavaScript";
+
+let text = "";
+for (let x of language) {
+text += x;
+}*/
+
+
+/*const numbers = [45, 4, 9, 16, 25];
+
+let txt = "";
+numbers.forEach(myFunction);
+
+function myFunction(value) {
+  txt += value;
+}
+console.log(numbers)*/
+
+/*var a= 10;
+for(var i=0;i<a.length;i=i+1){
+console.log(a[i])}*/
